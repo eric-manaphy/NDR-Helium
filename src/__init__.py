7916af62800cc5f3629617c1724627e4a0429d0d
Wei Chuan Wu <weichuw5@uci.edu>
2026-05-10 14:54:21 -0700

    a lot of update

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,3 @@
 from .basis import BasisFunction, make_basis
-from .one_e_integral import overlap_integral, potential_integral, kinetic_integral
-from .one_e_matrice import H_matrix, S_matrix, T_matrix, V_matrix
-
-from .two_e_integrals import (
-    Radial_repulsion_integral,
-    gaunt_matrix_element,
-    angular_part,
-    electron_repulsion_integral,
-)
-
-from .eri import build_eri_tensor
-from .JK_matrice import build_JK
-from .basis_group import group_basis_by_lm
-from .Density_occ import build_density, fractional_occupations
-
-from .scf import scf
+from .utils import pack_params, unpack_params, ao_to_mo_transform
+from .solver import scf, optimize_hf_orbitals
--- src/basis.py
+++ src/basis.py
@@ -16,3 +16,22 @@ def make_basis(*zeta_lists):
             for m in range(-l, l + 1):
                 bf.append(BasisFunction(zeta, l, m))
     return bf
+
+def group_basis_by_lm(basis):
+    lm_indices = {}
+    for i, bf in enumerate(basis):
+        lm_indices.setdefault((bf.l, bf.m), []).append(i)
+
+    l_values = sorted(set(bf.l for bf in basis))
+
+    radial_indices = {}
+    for l in l_values:
+        if (l, 0) not in lm_indices:
+            raise ValueError(f"No m=0 function for l = {l}")
+        radial_indices[l] = lm_indices[(l, 0)]
+
+        n_rad = len(radial_indices[l])
+        for m in range(-l, l + 1):
+            assert len(lm_indices[(l, m)]) == n_rad
+
+    return l_values, radial_indices, lm_indices
